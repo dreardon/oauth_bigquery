8ba590b5f336c02ac7188a9064a00a7680ee4e87
Daniel Reardon <danielreardon@google.com>
2024-02-22 16:15:39 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,143 @@
+const express = require("express");
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const {BigQuery} = require('@google-cloud/bigquery');
+const { google } = require('googleapis');
+const url = require('url');
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+
+require('dotenv').config()
+
+var app = express();
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/static'));
+
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(session({secret: process.env.SESSION_SECRET, resave: true, saveUninitialized: true}));
+
+
+const oauth2Client = new google.auth.OAuth2(
+  process.env.CLIENT_ID,
+  process.env.CLIENT_SECRET,
+  process.env.CALLBACK_URL,
+);
+
+const scopes = 'https://www.googleapis.com/auth/bigquery.readonly'
+
+const authorizationUrl = oauth2Client.generateAuthUrl({
+  access_type: 'offline',
+  scope: scopes,
+  include_granted_scopes: true,
+  prompt: 'consent',
+});
+
+
+function transformResponse(response) {
+  const fieldNames = ['unique_id', 'firstname', 'lastname', 'email'];
+
+  return response.map(row => {
+    const rowObject = {};
+    row.f.forEach((field, index) => {
+      rowObject[fieldNames[index]] = field.v;
+    });
+    return rowObject;
+  });
+}
+
+app.get('/',
+  function(req, res) {
+    res.render('pages/index', {auth_url: authorizationUrl});
+  }
+);
+
+app.get('/results',
+  async function(req, res) {
+    const ACCESS_TOKEN = req.session.accessToken;
+    console.log('ACCESS_TOKEN:' + ACCESS_TOKEN)
+
+    const DATASET_ID = process.env.DATASET_ID
+    const PROJECT_ID = process.env.PROJECT_ID
+    const TABLE_ID = process.env.TABLE_ID
+
+    const rows = await queryBigQueryWithREST(ACCESS_TOKEN,DATASET_ID,PROJECT_ID,TABLE_ID)
+    //const rows_sdk = await queryBigQueryWithSDK(ACCESS_TOKEN,DATASET_ID,PROJECT_ID,TABLE_ID) 
+
+    console.log('Returned Rows: ' + JSON.stringify(rows))
+    //console.log('Returned Rows_sdk: ' + JSON.stringify(rows_sdk))
+    res.render('pages/results', {token: ACCESS_TOKEN, results: rows });
+  }
+);
+
+async function queryBigQueryWithREST(ACCESS_TOKEN,DATASET_ID,PROJECT_ID,TABLE_ID) {
+  const query = `SELECT unique_id, firstname, lastname, email FROM \`${PROJECT_ID}.${DATASET_ID}.${TABLE_ID}\``;
+  const url = `https://bigquery.googleapis.com/bigquery/v2/projects/${PROJECT_ID}/queries`;
+  const options = {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${ACCESS_TOKEN}`,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      query: query,
+      location: 'US',
+      useLegacySql: false
+    })
+  };
+
+  try {
+    const response = await fetch(url, options);
+    const data = await response.json();
+    return transformResponse(data.rows) 
+  } catch (error) {
+      console.error('Error running query:', error);
+    throw error;
+  }
+}
+
+async function queryBigQueryWithSDK(ACCESS_TOKEN,DATASET_ID,PROJECT_ID,TABLE_ID) {
+  const query = `SELECT unique_id, firstname, lastname, email FROM \`${PROJECT_ID}.${DATASET_ID}.${TABLE_ID}\``;
+  
+  const bigqueryClient = new BigQuery({
+  projectId: `${PROJECT_ID}`});
+
+  const options = {
+      query: query,
+      location: 'US',
+      useLegacySql: false,
+      headers: {
+          'Authorization': `Bearer ${ACCESS_TOKEN}`,
+      }
+  };
+  console.log(JSON.stringify(options))
+
+  const [rows] = await bigqueryClient.query(options);
+
+  console.log('Query Results:');
+  rows.forEach(row => {
+    console.log(`row: ${row}`);
+  });
+}
+
+// Route to clear session
+app.get('/logout', (req, res) => {
+  req.session.destroy(err => {
+    if (err) {
+      res.status(500).send("Error clearing session");
+    } else {
+      res.redirect('/');
+    }
+  }); 
+});
+
+//general handlers
+app.use(function(err, req, res, next) {
+  console.log("Fatal error: " + JSON.stringify(err));
+  next(err);
+});
+
+var server = app.listen(8080, function () {
+  console.log('Listening on port %d', server.address().port)
+});
---
+++ readme.md
@@ -0,0 +1,128 @@
+# Simple OIDC OAuth With BigQuery Row-level Restriction
+This will setup a simple web application which will prompt a user to log in and display their oauth access token as well as use that access token on the server side to query BigQuery data with row-level restrictions enabled for two users. In this case:
+- mega-dev@example.com
+- mega@example.com
+
+## Google Disclaimer
+This is not an officially supported Google product
+
+## Setup Environment
+```BASH
+#Setup Environment variables
+export ORGANIZATION_ID=[ORG_ID]
+export PROJECT_NAME=[PROJECT_NAME]
+export REGION=[REGION]
+export BILLING_ACCOUNT=[BILLING_ID]
+
+printf 'Y' | gcloud projects create --name=$PROJECT_NAME --organization=$ORGANIZATION_ID
+export PROJECT_ID=$(gcloud projects list --filter=name:$PROJECT_NAME --format 'value(PROJECT_ID)')
+export PROJECT_NUMBER=$(gcloud projects list --filter=name:$PROJECT_NAME --format 'value(PROJECT_NUMBER)')
+gcloud beta billing projects link $PROJECT_ID --billing-account=$BILLING_ACCOUNT
+
+gcloud config set compute/region $REGION
+gcloud config set project $PROJECT_ID
+
+printf 'y' |  gcloud services enable orgpolicy.googleapis.com
+printf 'y' |  gcloud services enable compute.googleapis.com 
+printf 'y' |  gcloud services enable artifactregistry.googleapis.com
+printf 'y' |  gcloud services enable cloudbuild.googleapis.com
+printf 'y' |  gcloud services enable bigquery.googleapis.com
+```
+
+## Address Any Environment Restrictions (Optional)
+```BASH
+cat > allowedPolicyMemberDomains.yaml << ENDOFFILE
+name: projects/$PROJECT_NUMBER/policies/iam.allowedPolicyMemberDomains
+spec:
+  rules:
+  - allowAll: true
+ENDOFFILE
+gcloud org-policies set-policy allowedPolicyMemberDomains.yaml
+```
+
+### Establish Sample Big Query Data and Restriction
+```BASH
+bq --location=US mk -d \
+    --default_table_expiration 3600 \
+    --description "This is a sample dataset" \
+    sample_data
+
+cat > schema.json << ENDOFFILE
+[
+    {
+        "name": "unique_id",
+        "type": "INTEGER",
+        "mode": "REQUIRED"
+    },
+    {
+        "name": "firstname",
+        "type": "STRING",
+        "mode": "REQUIRED" 
+    },
+    {
+        "name": "lastname",
+        "type": "STRING",
+        "mode": "REQUIRED"
+    }
+    ,
+    {
+        "name": "email",
+        "type": "STRING",
+        "mode": "REQUIRED"
+    } 
+]
+ENDOFFILE
+
+bq mk \
+    --table \
+    --schema schema.json \
+    --expiration 864000 \
+    sample_data.names 
+
+cat > data.csv << ENDOFFILE
+1,Alice,Smith,mega@example.com
+2,Bob,Johnson,mega@example.com
+3,Charlie,Lee,mega@example.com
+4,David,Brown,mega@example.com
+5,Emily,Davis,mega@example.com
+6,Frank,Wilson,mega-dev@example.com
+7,Grace,Miller,mega-dev@example.com
+8,Henry,Clark,mega-dev@example.com
+9,Isabella,Garcia,mega-dev@example.com
+10,Jack,Martinez,mega-dev@example.com
+ENDOFFILE
+
+bq load \
+  --source_format=CSV \
+ sample_data.names \
+  ./data.csv
+
+CREATE ROW ACCESS POLICY user_row_filter
+ON sample_data.names
+GRANT TO ('domain:example.com')
+FILTER USING (email = SESSION_USER());
+
+#DROP ALL ROW ACCESS POLICIES user_row_filter
+#ON sample_data.names
+```
+
+## Deploy Application
+```BASH
+export SERVICE_NAME=oauthexample
+gcloud run deploy $SERVICE_NAME \
+   --source . \
+   --region $REGION \
+   --allow-unauthenticated
+
+DEPLOYMENT_URL=$(gcloud run services describe $SERVICE_NAME --region $REGION --format 'value(status.url)')
+
+gcloud run services update $SERVICE_NAME \
+   --region $REGION \
+   --update-env-vars "SESSION_SECRET=[NODEJS_SECRET]" \
+   --update-env-vars "CLIENT_SECRET=[OAUTH_CLIENT_SECRET]" \
+   --update-env-vars "CLIENT_ID=[OAUTH_CLIENT_ID]" \
+   --update-env-vars "DATASET_ID=sample_data" \
+   --update-env-vars "PROJECT_ID=[PROJECT_ID]" \
+   --update-env-vars "TABLE_ID=names" \
+   --update-env-vars "CALLBACK_URL=$DEPLOYMENT_URL/oauth/callback"
+```
